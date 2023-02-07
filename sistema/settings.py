2a52d5bbfe480e823293d99e1202209fa9649a21
Marcelo-MDQ <mmenendez@hotmail.com>
2023-02-06 21:28:15 -0300

    debug en false

--- sistema/settings.py
+++ sistema/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(!m1(#qsrm86(qzsi$owwqsnqyj_ovzp*rhw7uygzvon8vf1!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://marcelomdq.pythonanywhere.com/']
 
