62eea85ba4ed3070614107242232aaf18b540c40
Marcelo-MDQ <mmenendez@hotmail.com>
2023-09-05 20:45:48 -0300

    VARIAS CAJITAS

--- sistema/settings.py
+++ sistema/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(!m1(#qsrm86(qzsi$owwqsnqyj_ovzp*rhw7uygzvon8vf1!q'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!#DEBUG = True
 #DEBUG = True
 #ALLOWED_HOSTS = ['localhost']
 
